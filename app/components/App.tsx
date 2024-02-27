a519a2040d932a31786814edfbdc14ec8bcc9025
fancilivain <meigangwen@gmail.com>
2024-02-27 18:23:30 +0800

    managed to add level animations

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -2,24 +2,37 @@
 
 // import modules
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Sky, Environment,  MapControls } from "@react-three/drei";
+import { OrbitControls, Sky, Environment} from "@react-three/drei";
 import { useEffect, useState } from 'react'
+import { useControls } from "leva";
+import { MotionConfig } from "framer-motion";
+
+// import config
+import { framerMotionConfig } from "../config";
 
 // import components
 import Stack from "./Stack";
 
-
 export default function App() {
-     
+    
+    //declare the UI parameters
+    const { expanded } = useControls("Controls", {
+        expanded: false,
+    });
+
     return (
-        <Canvas shadows camera={{ position: [3, 3, 10], fov: 40 }}>
-            <color attach="background" args={["#ececec"]} />
-            <Stack />
-            
-            <ambientLight intensity={0.3} />
-            <OrbitControls />
-            <Sky />
-            <Environment preset="city" />
-        </Canvas>
+        <>
+            <MotionConfig transition={{...framerMotionConfig}}>
+                <Canvas shadows camera={{ position: [3, 20, 20], fov: 40 }}>
+                    <color attach="background" args={["#ececec"]} />
+                    <Stack expanded={expanded} />
+                    
+                    <ambientLight intensity={0.3} />
+                    <OrbitControls />
+                    <Sky />
+                    <Environment preset="city" />
+                </Canvas>
+            </MotionConfig>
+        </>
     )
 }
--- app/components/Level.tsx
+++ app/components/Level.tsx
@@ -1,10 +1,37 @@
+import { motion } from "framer-motion-3d";
+import { framerMotionConfig } from "../config";
+//import { animate, useMotionValue } from "framer-motion";
+
 export default function Level(props) {
+    const {position, index, distance, section, levels} = props;
+    
     return (
-        <group {...props}>
+        <motion.group 
+            position = {position}
+            animate={"" + section}
+            variants={{
+                0: {
+                    y: position[1],
+                    transition: {
+                        duration: 0.6,
+                        delay: index * 0.1,
+                        ...framerMotionConfig,
+                    },
+                },
+                1: {
+                    y: position[1] + distance * index,
+                    transition: {
+                        duration: 0.6,
+                        delay: (levels - index) * 0.1,
+                        ...framerMotionConfig,
+                    },
+                },
+            }}
+        >
             <mesh>
                 <boxGeometry args={[6.0,1.0,6.0]} />
                 <meshStandardMaterial envMapIntensity={0.5}/>
             </mesh>
-        </group>
+        </motion.group>
     )
 }
