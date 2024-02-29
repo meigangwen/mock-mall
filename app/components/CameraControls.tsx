2836b309cf038062e55397059a85bbaa5601197f
fancilivain <meigangwen@gmail.com>
2024-02-29 12:53:53 +0800

    finished the camera controls

--- app/components/CameraControls.tsx
+++ app/components/CameraControls.tsx
@@ -13,7 +13,7 @@ export default function CameraControls() {
     
     const { camera } = useThree(); 
     const { mode, focusedLevel } = useMallStore();    
-    const { cameraPositionArray, cameraLookAtArray } = useCameraStore();
+    const { isAnimating, duration, cameraPositionArray, cameraLookAtArray, setIsAnimating } = useCameraStore();
     const lookAtTargetPosition = cameraLookAtArray[mode];
 
     // create use motion values
@@ -32,24 +32,41 @@ export default function CameraControls() {
         cameraPositionZ.set(camera.position.z);
         
         // create camera animations
-        animate(cameraPositionX, cameraPositionArray[mode].x, { duration:1.0, });
-        animate(cameraPositionY, cameraPositionArray[mode].y, { duration:1.0, });
-        animate(cameraPositionZ, cameraPositionArray[mode].z, { duration:1.0, });
+        animate(cameraPositionX, cameraPositionArray[mode].x, { duration:duration, });
+        animate(cameraPositionY, cameraPositionArray[mode].y, { duration:duration, });
+        animate(cameraPositionZ, cameraPositionArray[mode].z, { duration:duration, });
+
+        // create look at animations
+        animate(cameraLookAtX, cameraLookAtArray[mode].x, { duration:duration, });
+        animate(cameraLookAtY, cameraLookAtArray[mode].y, { duration:duration, });
+        animate(cameraLookAtZ, cameraLookAtArray[mode].z, { duration:duration, });
          
     },[mode, focusedLevel]);
 
     // animate the camera
     useFrame((state) => {
-        if (cameraPositionY.isAnimating() || cameraPositionY.isAnimating() || cameraPositionZ.isAnimating())
+        if  (    cameraPositionY.isAnimating() 
+              || cameraPositionY.isAnimating() 
+              || cameraPositionZ.isAnimating()
+              || cameraLookAtX.isAnimating()
+              || cameraLookAtY.isAnimating()
+              || cameraLookAtZ.isAnimating()
+            )
         {
-            state.camera.position.set(cameraPositionX.get(),cameraPositionY.get(), cameraPositionZ.get())
+            state.camera.position.set(cameraPositionX.get(),cameraPositionY.get(), cameraPositionZ.get());
+            state.camera.lookAt(cameraLookAtX.get(), cameraLookAtY.get(), cameraLookAtZ.get());
+            setIsAnimating(true);
+        } else {
+            setIsAnimating(false);
         }
-        //state.camera.lookAt(0, cameraLookAtY.get(), cameraLookAtZ.get());
     });
 
     return (
         <>
-            <OrbitControls />
+            <OrbitControls
+                target = {lookAtTargetPosition}
+                enabled = {!isAnimating} 
+            />
         </>
     )
 }
--- app/state/cameraStore.ts
+++ app/state/cameraStore.ts
@@ -3,14 +3,22 @@ import * as THREE from 'three';
 
 type cameraStore = {
     //animate: boolean; // true is the camera is moving by animation
+    isAnimating: boolean;
+    duration: number;
     cameraPositionArray: THREE.Vector3[];  // 0 collaped, 1 expanded, 2 focused on a single level       
     cameraLookAtArray: THREE.Vector3[];
+
+    setIsAnimating: (isAnimating: boolean) => void;
 };
 
 const useCameraStore = create<cameraStore>((set) => ({
     //animate: false,
+    isAnimating: false,
+    duration: 1.0,
     cameraPositionArray: [new THREE.Vector3(0,10,30), new THREE.Vector3(0,15,40), new THREE.Vector3(0,10,20)],
-    cameraLookAtArray: [new THREE.Vector3(0,0,0), new THREE.Vector3(0,5,0), new THREE.Vector3(0,0,0)]
+    cameraLookAtArray: [new THREE.Vector3(0,0,0), new THREE.Vector3(0,5,0), new THREE.Vector3(0,0,0)],
+
+    setIsAnimating: (isAnimating) => set((state) => ({ isAnimating: isAnimating })),
 }));
 
 export default useCameraStore;
